a4f327a596a01ca221b64f25e46a6af0707fd31e
Миша Език <mishunder9354@gmail.com>
2024-12-08 21:09:09 +0200

    htmx fix

--- src/main/java/com/mixfa/docx_checker_web/docxchecker/documentchecker/AddonsChecker.java
+++ src/main/java/com/mixfa/docx_checker_web/docxchecker/documentchecker/AddonsChecker.java
@@ -6,17 +6,16 @@ import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 import org.springframework.stereotype.Component;
 import org.thymeleaf.util.StringUtils;
 
+import java.util.List;
 import java.util.function.Predicate;
 import java.util.regex.Pattern;
 
 @Component
 public class AddonsChecker implements ElementChecker<XWPFParagraph> {
-    private final static Predicate<String> ADDON_CHAR_PATTERN = Pattern.compile(
-            "^(Додаток|ДОДАТОК) [АБВГДЕЖКЛМНПРСТУФХЦШЩЮ]\n"
-    ).asPredicate();
+    private final static Predicate<String> ADDON_CHAR_PATTERN = Pattern.compile("^(Додаток|ДОДАТОК) [АБВГДЕЖКЛМНПРСТУФХЦШЩЮ]").asPredicate();
 
+    private static final List<String> ADDON_STYLES = List.of("Heading1", "1", "Heading1,1_Topic");
 
-    private static final String ADDON_TITLE_STYLE = "Heading1";
     private static final String ADDON_TITLE_NOT_MATCHES = "addontitleinvalid";
     private static final String ADDON_STYLE_NOT_MATCHES = "addonstyleinvalid";
 
@@ -24,15 +23,13 @@ public class AddonsChecker implements ElementChecker<XWPFParagraph> {
     public void checkElement(XWPFParagraph paragraph, ErrorsCollector errorsCollector) {
         var paragraphTextLC = paragraph.getText().toLowerCase();
         var paragraphText = paragraph.getText();
-        if (!paragraphTextLC.startsWith("додаток"))
-            return;
+        if (!paragraphTextLC.startsWith("додаток")) return;
 
+        if (!ADDON_CHAR_PATTERN.test(paragraphText)) errorsCollector.addError(ADDON_TITLE_NOT_MATCHES, paragraphText);
 
-        if (!ADDON_CHAR_PATTERN.test(paragraphText))
-            errorsCollector.addError(ADDON_TITLE_NOT_MATCHES, paragraphText);
-
-        if (!StringUtils.equals(ADDON_TITLE_STYLE, paragraph.getStyle()))
-            errorsCollector.addError(ADDON_STYLE_NOT_MATCHES);
+        var paragraphStyle = paragraph.getStyle();
+        var styleMatches = ADDON_STYLES.stream().anyMatch((style) -> StringUtils.equals(style, paragraphStyle));
+        if (!styleMatches) errorsCollector.addError(ADDON_STYLE_NOT_MATCHES);
     }
 
     @Override
